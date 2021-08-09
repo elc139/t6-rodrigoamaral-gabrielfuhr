bfb3f60391a6e91faf584f4b6581e4408eec0237
Gabriel Tobias Fuhr <gt.fuhr@gmail.com>
2021-08-09 20:56:01 -0300

    Atualiza benchmark

--- fractal/_benchmark.sh
+++ fractal/_benchmark.sh
@@ -10,7 +10,7 @@ mpirun -n 3 fractalpar 1024 64 >> results_2.csv
 mpirun -n 3 fractalpar 1024 400 >> results_2.csv
 mpirun -n 4 fractalpar 1024 32 >> results_2.csv
 mpirun -n 4 fractalpar 1024 64 >> results_2.csv
-mpirun --use-hwthread-cpus -n 8 fractalpar 1024 400 >> results_2.csv
+mpirun -n 4 fractalpar 1024 400 >> results_2.csv
 mpirun --use-hwthread-cpus -n 8 fractalpar 1024 32 >> results_2.csv
 mpirun --use-hwthread-cpus -n 8 fractalpar 1024 64 >> results_2.csv
 mpirun --use-hwthread-cpus -n 8 fractalpar 1024 400 >> results_2.csv
